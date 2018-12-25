5059d2691f580139fa1009eb105e73fb3ecbd3c7
simplebottest <46133286+simplebottest@users.noreply.github.com>
2018-12-25 17:03:01 +0200

    Update probot.js

--- probot.js
+++ probot.js
@@ -38,8 +38,7 @@ client.on("message", message => {
     
     if (message.guild) {
       score = client.getScore.get(message.author.id, message.guild.id);
-      if (!score) {
-        score = { id: `${message.guild.id}-${message.author.id}`, user: message.author.id, guild: message.guild.id, points: 0, level: 1 };
+      if (!score) {score = { id: `${message.guild.id}-${message.author.id}`, user: message.author.id, guild: message.guild.id, points: 0, level: 1 };
       }
       score.points++;
       const curLevel = Math.floor(0.1 * Math.sqrt(score.points));
